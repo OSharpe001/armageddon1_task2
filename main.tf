d833d5299534521b704cea43e183e0ca47c11ce9
O. Sharpe <osharpe001@gmail.com>
2024-05-27 10:36:26 -0400

    added task instructions

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+# Create a publically accessible web page with Terraform.  You must complete the following
+# 1) Terraform Script with a VPC
+# 2) Terraform script must have a VM within your VPC.
+# 3) The VM must have the homepage on it.
+# 4) The VM must have a publicly accessible link to it.
+# 5) You must Git Push your script to your Github.
+# 6) Output file must show 1) Public IP, 2) VPC, 3) Subnet of the VM, 4) Internal IP of the VM.
+
 terraform {
   required_providers {
     google = {
